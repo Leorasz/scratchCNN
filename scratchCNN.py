d24e9947336ca3b66b9eaf05d5eed384a439e83a
Leorasz <scaevola1337@duck.com>
2023-11-11 08:53:05 -0700

    Just to save

--- scratchCNN.py
+++ scratchCNN.py
@@ -3,6 +3,8 @@ from mnist import MNIST
 import matplotlib.pyplot as plt
 import math
 
+np.random.seed(1337)
+
 mndata = MNIST("Number_Samples")
 
 xTrain, yTrain = mndata.load_training()
@@ -12,15 +14,29 @@ xTrain = np.array(xTrain)/255
 xTest = np.array(xTest)/255
 class ConvolutionalNeuralNetwork():
     
-    def __init__(self, arch):
-        self.kernels = np.random.uniform(-1,1,(16,3,3))
+    def __init__(self, insize, convarch, arch):
+        self.kernels = [] #list of kernels, [numberkernels, crosssize, poolsize]
+        self.convarch = convarch
+        numouts = 1
+        outsize = insize
+        self.outsizes = [outsize]
+        self.poutsizes = []
+        for i in convarch:
+            self.kernels.append(np.random.uniform(-1,1,(i[0],i[1],i[1])))
+            numouts *= i[0]
+            poutsize = [outsize[0]-i[1]+1, outsize[1]-i[1]+1]
+            outsize = [math.ceil(j/i[2]) for j in poutsize]
+            self.outsizes.append(outsize)
+            self.poutsizes.append(poutsize)
+        arch.insert(0, numouts*outsize[0]*outsize[1])
         self.weights = []
         self.biases = []
+        self.outputSize = arch[-1]
         self.size = len(arch) - 1
         for i in range(1, len(arch)):
             fan_in = arch[i-1]
             fan_out = arch[i]
-            # Xavier initialization for weights
+            #Xavier for weights
             limit = np.sqrt(7 / (fan_in + fan_out))
             self.weights.append(np.random.uniform(-limit, limit, (fan_out, fan_in)))
             self.biases.append(np.random.rand(arch[i], 1) - 0.5)
@@ -28,33 +44,44 @@ class ConvolutionalNeuralNetwork():
     def ReLU(self, x):
         return np.maximum(x, 0)
 
-    def ReLUd(self, x):
+    def ReLUd(self, x): #derivative of ReLU
         return x > 0
 
     def sigmoid(self, z):
         return 1/(1+np.exp(-(z)))
 
-    def sigmoidd(self, x):
+    def sigmoidd(self, x): #derivative of sigmoid
         return self.sigmoid(x) * (1.0 - self.sigmoid(x))
     
-    def oneHotter(self, x):
-        new = np.zeros((10,1))
+    def oneHotter(self, x): #one hot encoder for output
+        new = np.zeros((self.outputSize,1))
         new[x] = 1
         return new
     
     def loss(self, y_pred, y_true):
-        epsilon = 1e-7  # small value to avoid division by zero
+        epsilon = 1e-7  #just so no division by zero
         loss = -np.sum(y_true.reshape(len(y_true)) * np.log(y_pred.reshape(len(y_pred)) + epsilon))
         return loss
     
-    def convolve(self, x, f):
+    def validCrossCorr(self, x, f, pp=False):
+        if pp:
+            print(x)
+            print(f)
         res = np.zeros((len(x)-len(f)+1, len(x[0])-len(f[0])+1))
         for i in range(len(x)-len(f)+1):
             for j in range(len(x[0])-len(f[0])+1):
                 res[i][j] = np.sum(np.multiply(f, x[i:i+len(f),j:j+len(f[0])]))
         backz = np.copy(res)
         res = [[self.ReLU(j) for j in i] for i in res]
+        backz = [[self.ReLUd(j) for j in i] for i in backz]
         return res, backz
+    
+    def fullConvolve(self, d, f):
+        newf = np.flip(f, (0,1))
+        padding_width = ((len(f)-1,len(f)-1),(len(f[0])-1,len(f[0])-1))
+        newd = np.pad(d, pad_width=padding_width, mode="constant", constant_values=0)
+        res, _ = self.validCrossCorr(newd, newf)
+        return res
     
     def maxpool(self, x, s):
         res = np.zeros((math.ceil(len(x)/s), math.ceil(len(x[0])/2)))
@@ -74,22 +101,38 @@ class ConvolutionalNeuralNetwork():
                 ri = (i*s)+(pos[i,j]//s)
                 rj = (j*s)+(pos[i,j]%s)
                 res[int(ri), int(rj)] = x[i,j]
-
         return res
      
+    def softmax(self, x):
+        e = sum([np.exp(i) for i in x])
+        return np.array([np.exp(i)/e for i in x])
+    
     def forward(self, x):
         self.ps = []
-        ksout = []
-        self.backz = []
-        for kernel in self.kernels: #for every kernel 
-            p, z = self.convolve(x, kernel)
-            self.backz.append(z)
-            y, ps = self.maxpool(np.array(p), 2)
-            ksout.append(y)
-            self.ps.append(ps)
-
+        self.ksout = [[[x]]] # (layer, kernel, input, row, value)
+        self.backz = [] #store for backprop
+        for ln, layer in enumerate(self.kernels):
+            zl = [] #zstore for layer
+            pl = [] #posstore for layer
+            newks = []
+            for kernel in layer: #for every kernel 
+                zinp = []
+                pinp = []
+                ksi = []
+                for inp in self.ksout[-1]: #for every input from the previous layer
+                    p, z = self.validCrossCorr(inp, kernel) #product and zstore of the crosscorrelation
+                    zinp.append(z) #intermediate zstore
+                    y, ps = self.maxpool(np.array(p), self.convarch[ln][2]) #maxpool the output, store positions
+                    ksi.append(y) #ksout add the output
+                    pinp.append(ps) #store the positions
+                zl.append(zinp)
+                pl.append(pinp)
+                newks.append(ksi)
+            self.ksout.append(newks)
+            self.backz.append(zl)
+            self.ps.append(pl)
         self.zs = []
-        self.aas = [np.array(ksout).reshape(2704,1)]
+        self.aas = [np.array(self.ksout[-1]).reshape(-1,1)]
         for i in range(self.size):
             self.zs.append(np.array([self.biases[i][jindex] + j for jindex, j in enumerate(self.weights[i].dot(self.aas[i]))]))#self.weights[i].dot(self.aas[i]) + self.biases[i])
             if i < self.size - 1:
@@ -97,15 +140,8 @@ class ConvolutionalNeuralNetwork():
             else:
                 self.aas.append(self.sigmoid(self.zs[i]))
     
-    def givOut(self, x):
-        self.forward(x.reshape(len(x),1))
-        return self.aas[-1]
-    
-    def softmax(self, x):
-        e = sum([np.exp(i) for i in x])
-        return np.array([np.exp(i)/e for i in x])
-    
-    def backward(self, x, y):
+    def backward(self, y):
+        print(np.array(self.ksout[0]).shape)
         self.dws = [0]*self.size
         self.dbs = [0]*self.size
         for i in range(self.size-1,-1,-1):
@@ -114,14 +150,28 @@ class ConvolutionalNeuralNetwork():
             else:
                 self.dbs[i] = self.weights[i+1].T.dot(self.dbs[i+1])*self.sigmoidd(self.zs[i])
             self.dws[i] = self.dbs[i].dot(np.array(self.aas[i]).T) #dL/dY*Xt (dactivate already factored in)
-        dLdP = []
-        dLdY = np.array(self.weights[0].T.dot(self.dbs[0])).reshape(16,13,13)
-        for i, p in zip(dLdY, self.ps):
-            dLdP.append(self.unpool(i, 2, p, 26, 26))
+
+        #now time for the convolutional layers
         self.dks = []
-        for kernel in dLdP: #for every kernel 
-            new, _ = self.convolve(x, kernel)
-            self.dks.append(new)
+        #dLdY is also dLdX
+        dLdY = np.array(self.weights[0].T.dot(self.dbs[0])).reshape(self.convarch[-1][0], -1, self.outsizes[-1][0],self.outsizes[-1][1])
+        for ln in range(len(self.kernels)-1,-1,-1): #for every layer, to backprop
+            dl = [] #stores dks for layer
+            dd = {} #stores dxs for layer, use dictionary because does the for loops but opposite
+            for ki, kernel in enumerate(dLdY): #for every kernel that acted on the last inputs
+                gradient = np.zeros(kernel[0].shape)
+                for ii, inp in enumerate(kernel): #for every input that was acted on by the kernels
+                    p = self.unpool(dLdY[ki][ii], self.convarch[ln][2], self.ps[ln][ki][ii], self.poutsizes[ln][0], self.poutsizes[ln][1])
+                    z = p*self.backz[ln][ki][ii]
+                    gradient += self.validCrossCorr(self.ksout[ln][ki][ii], z, pp=True) #gradient is sum of all of them
+                    if ln != 0: #no point if last layer
+                        #compute new dLdXs
+                        dd[ii] = dd.get(ii, np.zeros((self.outsizes[ln-1][0], self.outsizes[ln-1][1]))) + self.fullConvolve(z, self.kernels)
+                dl.append(gradient)
+            self.dks.append(dl)
+            if ln != 0: #sets new dxs
+                dLdY = np.array(list(dd.values())).reshape(self.convarch[ln-1][0], -1, self.outsizes[ln-1][0], self.outsizes[ln-1][1])
+                
 
     def update(self, lr):
         for i in range(self.size):
@@ -130,11 +180,12 @@ class ConvolutionalNeuralNetwork():
         for index, i in enumerate(self.kernels):
             for jindex, j in enumerate(i):
                 for kindex, k in enumerate(j):
-                    k -= self.dks[index][jindex][kindex]
+                    for lindex, l in enumerate(k):
+                        l -= self.dks[index][jindex][kindex][lindex]
 
     def descend(self, x, y, lr):
         self.forward(np.array(x).reshape(28, 28))
-        self.backward(np.array(x).reshape(28, 28), self.oneHotter(y))
+        self.backward(self.oneHotter(y))
         self.update(lr)
 
     def train(self, x, y, lr, iter):
@@ -170,7 +221,6 @@ class ConvolutionalNeuralNetwork():
         print("Testing begun")
         for i in range(iter):
             self.forward(np.array(x[i]).reshape(784, 1))
-            #print("Iteration number " + str(i))
             lossa = self.loss(self.softmax(self.aas[-1]),self.oneHotter(y[i]))
             if np.argmax(self.aas[-1]) == y[i]:
                 correct += 1
@@ -186,7 +236,7 @@ class ConvolutionalNeuralNetwork():
         print("The average loss in testing was " + str(loss/iter))
         return correct/iter 
 
-
-nn = ConvolutionalNeuralNetwork([2704,64,32,10])
+#[[amount of kernels, kernel size, pool size]]
+nn = ConvolutionalNeuralNetwork([28,28],[[16,3,2]],[64,32,10])
 nn.train(xTrain[:10], yTrain[:10], 0.01, 200)
 #print(str(nn.test(xTest,yTest,100)*100)+"%")
